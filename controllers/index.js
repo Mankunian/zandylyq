444013e693a00835e801a2774e218a126d03349f
d.aidarkhan <reddevils1878>
2019-05-23 03:56:48 +0600

    зан

--- controllers/index.js
+++ controllers/index.js
@@ -8,26 +8,55 @@ angular.module("app").controller("mainCtrl", function ($scope, $http) {
         {'id': 3, 'crimeName': 'Оконченное'}
     ];
 
-    console.log($scope.crimeStageList);
-
-    $scope.sendRequest = function () {
-
-    };
-
 
     $scope.myFunction = function () {
         console.log($scope.searchByNumber);
         $http({
             url: 'http://api.zandylyq.kz/v1/judgment/search-qualif-by-stat/',
-            // url: 'http://api.zandylyq.kz ',
             method: 'POST',
             data: {qualif_name: $scope.searchByNumber}
         }).then(function (data) {
-            console.log(data)
+            angular.forEach(data.data, function (value) {
+                console.log(value);
+                $scope.crimeList = value;
+            })
         }, function (error) {
             console.log(error)
         })
-    }
+    };
+
+
+    $scope.sendRequest = function () {
+
+        if ($scope.soft) {
+            $scope.soft = 1;
+        } else ($scope.soft = 0);
+        if ($scope.heavy) {
+            $scope.heavy = 1
+        } else ($scope.heavy = 0);
+
+        var today = new Date();
+        var sendBodyObj = {
+            'qualification': $scope.stage * 1,
+            'crime_date': today.toLocaleDateString(),
+            'article24': $scope.crime,
+            'gender': $scope.gender * 1,
+            'age': $scope.age,
+            'soft': $scope.soft,
+            'heavy': $scope.heavy
+        };
+        console.log(sendBodyObj);
+
+        $http({
+            url: 'http://api.zandylyq.kz/v1/judgment/request/',
+            method: 'POST',
+            data: sendBodyObj
+        }).then(function (value) {
+            console.log(value)
+        }, function (reason) {
+            console.log(reason)
+        })
+    };
 
 
 });
