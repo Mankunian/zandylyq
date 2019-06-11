6a32084b526e7ef1cc57c0265c75822d49bd94c2
d.aidarkhan <reddevils1878>
2019-06-11 17:29:51 +0600

    график по уголовному делу

--- controllers/index.js
+++ controllers/index.js
@@ -119,6 +119,8 @@ var modalContent = function ($scope, $uibModalInstance, $http, value, article) {
             'soft': value.soft ? 1 : 0,
             'heavy': value.heavy ? 1 : 0
         };
+
+        $scope.objForChart = sendBodyObj;
         $scope.loader = true;
         $http({
             url: 'http://api.zandylyq.kz/v1/judgment/request/?&nocache=2',
@@ -146,6 +148,143 @@ var modalContent = function ($scope, $uibModalInstance, $http, value, article) {
         $uibModalInstance.close();
     };
     $scope.getResponse();
+
+    $scope.showChart = function () {
+
+        console.log($scope.objForChart);
+
+        /* var object = {
+             "article_id": $scope.objForChart.article_id,
+             "article24_id": $scope.objForChart.article24_id,
+             "gender": $scope.objForChart.gender,
+             "age": $scope.objForChart.age,
+             "soft": $scope.objForChart.soft,
+             "heavy": $scope.objForChart.heavy
+         };*/
+
+        var object = {
+            "article_id": "1880002",
+            "article24_id": "3",
+            "gender": 2,
+            "age": 27,
+            "soft": 0,
+            "heavy": 0
+        };
+
+        $http({
+            method: 'POST',
+            url: 'http://api.zandylyq.kz/v1/stat/vid-nakaz/',
+            data: object,
+            cache: false,
+            contentType: false,
+            async: true,
+            processData: false,
+            headers: {
+                'Access-Control-Allow-Origin': true,
+                'Content-Type': 'application/json; charset=utf-8',
+                "X-Requested-With": "XMLHttpRequest"
+            }
+        }).then(function (value) {
+            $scope.data = value.data.result;
+            if ($scope.data === '') {
+                $scope.noData = 'Нет данных'
+            } else {
+                var series = [];
+                var i = 0;
+                var color = ['#50ADF5', '#13c632', '#FFCB45', '#6877e5', '#6FB07F'];
+
+                console.log($scope.data);
+                angular.forEach($scope.data, function (value) {
+                    var obj = {
+                        values: [+value.cntLic],
+                        text: value.VidNakaz,
+                        backgroundColor: color[i]
+                    };
+                    series.push(obj);
+                    console.log(obj);
+                    i++;
+                });
+
+                var myConfig = {
+                    type: "pie",
+                    backgroundColor: "#003849",
+                    // "background-color": "#003849",
+                    plot: {
+                        borderColor: "#fff",
+                        borderWidth: 5,
+                        // slice: 90,
+                        valueBox: {
+                            placement: 'out',
+                            // text: '%t\n%npv%',
+                            text: '%t\n%v',
+                            fontFamily: "Open Sans"
+                        },
+                        tooltip: {
+                            fontSize: '18',
+                            fontFamily: "Open Sans",
+                            padding: "5 10",
+                            text: "%v"
+                        },
+                        animation: {
+                            effect: 2,
+                            method: 5,
+                            speed: 500,
+                            sequence: 1
+                        }
+                    },
+                    plotOptions: {
+                        pie: {
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            point: {
+                                events: {
+                                    click: function () {
+                                        console.log('IT works');
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    source: {
+                        // text: 'gs.statcounter.com',
+                        fontColor: "#8e99a9",
+                        fontFamily: "Open Sans"
+                    },
+                    title: {
+                        fontColor: "#fff",
+                        text: 'Вид и сроки наказания',
+                        align: "center",
+                        offsetX: 10,
+                        fontFamily: "Open Sans",
+                        fontSize: 25,
+                        backgroundColor: "none"
+                    },
+                    subtitle: {
+                        offsetX: 10,
+                        offsetY: 10,
+                        fontColor: "#8e99a9",
+                        fontFamily: "Open Sans",
+                        fontSize: "13",
+                        text: 'Кол-во лиц',
+                        align: "left"
+                    },
+                    plotarea: {
+                        // margin: "20 0 0 0"
+                    },
+                    series: series
+                };
+
+                zingchart.render({
+                    id: 'pieChart',
+                    data: myConfig
+                    // height: 500,
+                    // width: 600
+                });
+            }
+        }, function (reason) {
+            console.log(reason)
+        });
+    };
 
 
     $scope.cancel = function () {
